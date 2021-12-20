40c4c667c1fd3d36d4e98637f73db4500fa0a847
Mateusz Trzuskowski <mateusz.trzuskowski.91@gmail.com>
2021-12-20 20:34:36 +0100

    Data cleaning

--- data_processing.py
+++ data_processing.py
@@ -4,3 +4,21 @@ import pandas as pd
 actors = pd.read_json('dataset.json')
 actors
 actors.info()
+
+act = actors.copy()
+act
+act.info(memory_usage='deep')
+
+act['dob'].count()
+act['dob'] = pd.to_datetime(act['dob'])
+act['dod'].count()
+act.loc[~act['dod'].isna()].count()
+act['dod'].value_counts()
+act.loc[act['dod'] == '\n']
+act.loc[act['dod'] == '\n'] = pd.n
+act['dod'] = pd.to_datetime(act['dob'])
+
+act['gender'].value_counts()
+act.loc[act['gender'] == 'm']
+act.loc[act['gender'] == 'm'] = 'M'
+act['gender'] = act['gender'].astype('category')
